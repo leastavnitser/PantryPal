2421dfbeaa46ae44176656f573d1a3d114ac390e
Lea Stavnitser <lea.stavnitser@gmail.com>
2024-04-18 13:18:39 -0500

    adds pantry items crud

--- app/controllers/pantry_items_controller.rb
+++ app/controllers/pantry_items_controller.rb
@@ -1,20 +1,40 @@
 class PantryItemsController < ApplicationController
   
+  
+  def index
+    @pantry_items = PantryItem.all
+    render :index
+  end
+
+  def show
+    @pantry_item = PantryItem.find_by(id: params[:id])
+    render :show
+  end
+  
   def create
-    @PantryItem = PantryItem.create(
-      name: params[:name],
-      image_url: params[:image_url]
+    @pantry_item = PantryItem.create(
+      ingredient_id: params[:ingredient_id],
+      user_id: params[:user_id],
+      amount: params[:amount]
     )
     render :show
   end
 
   def update
-    @pantryItem = PantryItem.find_by(id: params[:id])
-    @pantryItem.update(
-      name: params[:name] || @pantryItem.name,
-      image_url: params[:image_url] || @pantryItem.image_url
+    @pantry_item = PantryItem.find_by(id: params[:id])
+    @pantry_item.update(
+      ingredient_id: params[:ingredient_id] || @pantry_item.ingredient_id,
+      user_id: params[:user_id] || @pantry_item.user_id,
+      amount: params[:amount] || @pantry_item.amount
     )
     render :show
+  end
+
+
+  def destroy
+    @pantry_item = PantryItem.find_by(id: params[:id])
+    @pantry_item.destroy
+    render json: { message: "Pantry Item successfully removed" }
   end
 
 end
