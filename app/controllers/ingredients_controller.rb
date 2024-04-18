7e38a21c6013455b9efdf1ac8ac59cb3bca61206
Lea Stavnitser <lea.stavnitser@gmail.com>
2024-04-18 11:58:10 -0500

    adds controllers

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,34 @@
+class IngredientsController < ApplicationController
+  def index
+    @ingredients = Ingredient.all
+    render :index
+  end
+
+  def show
+    @ingredient = Ingredient.find_by(id: params[:id])
+    render :show
+  end
+  
+  def create
+    @ingredient = Ingredient.create(
+      name: params[:name],
+      image_url: params[:image_url]
+    )
+    render :show
+  end
+
+  def update
+    @ingredient = Ingredient.find_by(id: params[:id])
+    @ingredient.update(
+      name: params[:name] || @ingredient.name,
+      image_url: params[:image_url] || @ingredient.image_url
+    )
+    render :show
+  end
+
+  def destroy
+    @ingredient = Ingredient.find_by(id: params[:id])
+    @ingredient.destroy
+    render json: { message: "ingredient successfully removed" }
+  end
+end
