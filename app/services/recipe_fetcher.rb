28e0748f858173617f69507c51f069ac75591bd9
Lea Stavnitser <lea.stavnitser@gmail.com>
2024-05-07 14:27:39 -0500

    adds api website connection

--- app/services/recipe_fetcher.rb
+++ app/services/recipe_fetcher.rb
@@ -1,5 +1,9 @@
 class RecipesFetcher 
   include httparty
   base_uri 'https://tasty.p.rapidapi.com/recipes/list'
+  def self.fetch_data(article_id = '')
+    response = get("#{base_uri}/recipe")
+    puts "Debug: Fetch data response - #{response.inspect}"
+    resopoonse.parsed_response
   end
 end
